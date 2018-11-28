f89867c2ccf703991161cf859ea230b8ab56f33a
zhangmin <zhangmin89@outlook.com>
2018-11-28 11:37:55 +0800

    添加项目文件。

---
+++ WPFMVVMDemo.ViewModel/MainWindowsViewModel.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Infrastructure;
+
+namespace WPFMVVMDemo.ViewModel
+{
+    public class MainWindowsViewModel : NotifyObject
+    {
+        private String _disPlayText;
+
+        public string DisPlayText
+        {
+            get
+            {
+                return _disPlayText;
+            }
+            set
+            {
+                SetPropertyNotify(ref _disPlayText, value, nameof(DisPlayText));
+            }
+        }
+        
+        private readonly NVCommand _addCommand;
+        public NVCommand AddCommand
+        {
+            get => _addCommand;
+        }
+
+        private readonly NVCommand _subtractCommand;
+        public NVCommand SubtractCommand
+        {
+            get => _subtractCommand;
+        }
+
+        private readonly NVCommand _multiplyCommand;
+        public NVCommand MultiplyCommand
+        {
+            get => _multiplyCommand;
+        }
+
+        private readonly NVCommand _divideCommand;
+        public NVCommand DivideCommand
+        {
+            get => _divideCommand;
+        }
+
+        public MainWindowsViewModel()
+        {
+            _addCommand = new NVCommand(AddHandler);
+            _subtractCommand = new NVCommand(SubtractHandler);
+            _multiplyCommand = new NVCommand(MultiplyHandler);
+            _divideCommand = new NVCommand(DivideHandler);
+        }
+
+        private void AddHandler()
+        {
+            DisPlayText = "+";
+        }
+
+        private void SubtractHandler()
+        {
+            DisPlayText = "-";
+        }
+
+        private void MultiplyHandler()
+        {
+            DisPlayText = "*";
+        }
+
+        private void DivideHandler()
+        {
+            DisPlayText = "/";
+        }
+    }
+}
